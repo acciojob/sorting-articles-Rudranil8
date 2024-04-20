ac4978b8577ef989a5d28d4b4e685055e60d1c7d
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-20 09:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const bands=['The Plot in You', 'The Devil Wears Prada', 'Pierce The Veil', 'Norma Jean', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'Anything But Here', 'An Old Dog'];
+document.addEventListener('DOMContentLoaded', (event) => {
+    const bands=['The Plot in You', 'The Devil Wears Prada', 'Pierce The Veil', 'Norma Jean', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'Anything But Here', 'An Old Dog'];
 
-function strip(bandName){
-	return bandName.replace(/^(a |the |an)/i, '').trim();
-}
+    function strip(bandName){
+        return bandName.replace(/^(a |the |an )/i, '').trim();
+    }
 
-const sortedBands= bands.sort((a, b)=> strip(a)>strip(b) ? 1:-1);
-const bandList=document.getElementById('band');
+    const sortedBands= bands.sort((a, b)=> strip(a) > strip(b) ? 1 : -1);
+    const bandList = document.getElementById('band');
 
-sortedBands.forEach(band=>{
-	const li=document.createElement('li');
-	li.textContent=band;
-	bandList.appendChild(li);
-})
+    sortedBands.forEach(band=>{
+        const li = document.createElement('li');
+        li.textContent = band;
+        bandList.appendChild(li);
+    })
+});
